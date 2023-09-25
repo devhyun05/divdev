07319a305abe796f7f2a0d589ef112a467517b56
HyunSeongLee <devhyun05@gmail.com>
2023-09-25 16:10:27 -0400

    adding more global item for logged in user

--- frontend/src/context/LoginContext.jsx
+++ frontend/src/context/LoginContext.jsx
@@ -5,12 +5,22 @@ const LoginContext = createContext();
 export function LoginProvider({ children }) {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     const [loginText, setLoginText] = useState("sss");
+    const [userName, setUserName] = useState(""); 
+    const [profileLayout, setProfileLayout] = useState(); 
+    const [navbarLayout, setnavbarLayout] = useState(); 
+
     return (
         <LoginContext.Provider value={{
             isLoggedIn,
             setIsLoggedIn,
             loginText,
-            setLoginText
+            setLoginText,
+            userName,
+            setUserName,
+            profileLayout, 
+            setProfileLayout,
+            navbarLayout, 
+            setnavbarLayout 
         }}>
             {children}
         </LoginContext.Provider>
