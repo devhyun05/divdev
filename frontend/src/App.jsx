524a89a7ac254543d160a085a3b75142fb7f7b9c
HyunSeongLee <devhyun05@gmail.com>
2023-09-26 17:49:47 -0400

    added more pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,18 @@
 import './App.css';
 import NavigationComponent from './components/NavigationComponent';
 import Home from './pages/Home';
-import About from './pages/About';
-import Privacy from './pages/Privacy';
 import Login from './pages/Login';
 import Register from './pages/Register'; 
+import Profile from './pages/Profile';
+import Posts from './pages/Posts';
+import Settings from './pages/Settings';
+import Privacy from './pages/Privacy';
 import Terms from './pages/Terms'; 
 import { LoginProvider } from './context/LoginContext';  
-import { Route, Routes } from "react-router-dom"; 
+import { Route, Routes} from "react-router-dom"; 
 function App() {
+  
+ 
   return (
     <div className="App">
       <LoginProvider>
@@ -16,10 +20,12 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />}/>
           <Route path="/:username" element={<Home />}/>
-          <Route path="/about" element={<About />}/>
-          <Route path="/privacy" element={<Privacy />}/>
           <Route path="/login" element={<Login/>}/>
           <Route path="/register" element={<Register/>}/>
+          <Route path="/profile" element={<Profile/>}/> 
+          <Route path="/posts" element={<Posts/>}/> 
+          <Route path="/settings" element={<Settings/>}/>
+          <Route path="/privacy" element={<Privacy />}/>
           <Route path="/terms" element={<Terms/>}/>
         </Routes>
       </LoginProvider>
