f0335149ab01ff5f03d553162ac89c5df76a4b83
HyunSeongLee <devhyun05@gmail.com>
2023-10-05 19:04:08 -0400

    set user image

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,32 @@
+import React, { useEffect, useContext } from 'react'; 
+import LoginContext from '../context/LoginContext'; 
+
+const backend = 'http://localhost:3000';
 
 const Home = () => {
+    const { userName, setUserProfileImage } = useContext(LoginContext); 
 
+    useEffect(()=>{
+        fetchUserInfo();
+    }, []); 
+
+    const fetchUserInfo = async () => {
+        try {
+            await fetch(`${backend}/${userName}/set-image`, {
+                method: 'POST', 
+                headers: {
+                    'Content-Type': 'application/json' 
+                }, 
+                body: JSON.stringify({username: userName})
+            }).then(response => response.json())
+            .then(data => {
+                console.log(data);
+                setUserProfileImage(data.userImage); 
+            });
+        } catch (error) {
+            console.error('Error: ', error); 
+        }
+    }
     return (
         <>
         </>
