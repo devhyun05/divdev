b24324bb237010efe9c34b71705b8c617013cfb2
HyunSeongLee <devhyun05@gmail.com>
2023-10-07 21:57:38 -0400

    remove unnecessary icon

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react'; 
 import { useNavigate } from 'react-router-dom'; 
 import LoginContext from '../context/LoginContext'; 
-import Avatar from '@mui/material/Avatar';
+
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline'; 
 import TextField from '@mui/material/TextField'; 
@@ -10,7 +10,7 @@ import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box'; 
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined'; 
+
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider} from '@mui/material/styles'; 
