c6c57f6e7caf693f6e9c8566805f6b13b981ab28
HyunSeongLee <devhyun05@gmail.com>
2023-10-14 00:09:46 -0400

    file input added css

--- frontend/src/pages/AddPost.jsx
+++ frontend/src/pages/AddPost.jsx
@@ -9,6 +9,8 @@ import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box'; 
 import Button from '@mui/material/Button'
 
+import AddPhotoAlternateIcon from '@mui/icons-material/AddPhotoAlternate';
+
 const CssTextField = styled(TextField)({
     '& label.Mui-focused': {
       color: '#A0AAB4',
@@ -32,6 +34,9 @@ const CssTextField = styled(TextField)({
 
 const AddPost = () => {
     const [value, setValue] = useState('');
+    const [image, setImage] = useState(''); 
+    const [imageName, setImageName] = useState('');
+
     const toolbarOptions = [
                            ['bold', 'italic', 'underline', 'strike'], 
                            ['blockquote', 'code-block'],
@@ -54,9 +59,37 @@ const AddPost = () => {
         toolbar: toolbarOptions,
     }; 
 
+    const handleImageChange = (event) => {
+        setImage(event.target.files[0]);
+        setImageName(event.target.files[0].name); 
+    }; 
+
     return (
         <>
+   
             <Box sx={{display: 'flex', flexDirection: 'column',  width: '60%', height: '60%', margin: '0 auto', color: 'white'}}>
+                    
+                    <input type="file" id="file" onChange={handleImageChange}/> 
+                    <label for="file" class="file-label">
+                    {imageName ? (
+                        imageName
+                        ) : (
+                        <>
+                            <AddPhotoAlternateIcon />
+                            Choose Thumbnail
+                        </>
+                        )}           
+                    </label>
+              
+
+                {/* <FormControl fullWidth>
+                  
+                    <Select                        
+                    >
+                        <MenuItem value={"React"}>React</MenuItem>
+                        <MenuItem value={"Angular"}>Angular</MenuItem>
+                    </Select>
+                </FormControl> */}
                 <CssTextField
                     sx={{marginBottom: '5%'}}
                     placeholder="Title"
@@ -66,6 +99,7 @@ const AddPost = () => {
                         },                            
                     }}
                 />
+
                 <ReactQuill
                     modules={module}
                     theme="snow"
@@ -76,7 +110,10 @@ const AddPost = () => {
             </Box>
             <AppBar component="nav" sx={{position: 'fixed', bottom: '0', top: '91%', background:'#1e1e1e'}}>
                 <Toolbar sx={{display: 'flex', justifyContent: 'flex-end'}}>
-                    <Button sx={{color: 'black', backgroundColor: '#F5F5DC', borderRadius: '10px', width: '100px', marginTop: '10px'}}>
+                    <Button sx={{color: 'black', backgroundColor: '#F5F5DC', borderRadius: '10px', width: '100px', marginTop: '1%', marginRight: '1%'}}>
+                        Preview
+                    </Button>
+                    <Button sx={{color: 'black', backgroundColor: '#F5F5DC', borderRadius: '10px', width: '100px', marginTop: '1%'}}>
                         Post
                     </Button>
                 </Toolbar>
