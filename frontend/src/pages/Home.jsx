1b2a11958f8f3ed20ff890ddc62d7faf4b4626b1
HyunSeongLee <devhyun05@gmail.com>
2023-10-15 13:16:09 -0400

    render user posts

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,29 +1,27 @@
+import "../App.css"; 
 import React, { useState, useEffect, useContext } from 'react'; 
 import LoginContext from '../context/LoginContext';
-import { Link } from 'react-router-dom'; 
-import Container from '@mui/material/Container'; 
+import { Link, useNavigate } from 'react-router-dom'; 
 import Box from '@mui/material/Box'; 
 import Button from '@mui/material/Button'; 
-import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography'; 
-import AddIcon from '@mui/icons-material/Add';
-
+import Avatar from '@mui/material/Avatar'; 
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import CommentIcon from '@mui/icons-material/Comment';
 
 const backend = 'http://localhost:3000';
 
 const Home = () => {
 
-    const { userName,  setUserProfileImage } = useContext(LoginContext); 
-
-
+    const { userName, userProfileImage, setUserProfileImage } = useContext(LoginContext); 
+    const [blogPostInfo, setBlogPostInfo] = useState([]); 
+    const navigate = useNavigate(); 
     useEffect(()=>{
         fetchUserInfo();
+        fetchBlogPost(); 
     }, []); 
 
     const fetchUserInfo = async () => {
-      
-        // const username = window.localStorage.getItem('username'); 
-        // setUserName(username); 
         try {
             await fetch(`${backend}/${userName}/set-image`, {
                 method: 'POST', 
@@ -33,7 +31,6 @@ const Home = () => {
                 body: JSON.stringify({username: userName})
             }).then(response => response.json())
             .then(data => {
-                
                 setUserProfileImage(data.userImage); 
             });
         } catch (error) {
@@ -41,35 +38,64 @@ const Home = () => {
         }
     }
 
-    const handleAddCategory = () => {
-        
-    };
+    const fetchBlogPost = async () => {
+        try {
+            console.log("called");
+            console.log(userName);
+            const response = await fetch(`${backend}/${userName}/post/get-blog-post`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({username: userName})
+            })
+            const posts = await response.json(); 
+            
+            setBlogPostInfo(posts);             
+          
+        } catch (error) {
+            console.error(error); 
+        }
+    }; 
+
+    const handleNavigatePostDetails = (postTitle) => {
+        navigate(`/${userName}/${postTitle}`);
+    }; 
 
     return (
         <>
-            <Box sx={{display: 'flex', flexDirection: 'row', gap: '100px'}}>
-                <Box sx={{marginTop: '5%'}}>
-                    {/* <Button sx={{color: 'white'}}
-                            onClick={handleAddCategory}>
-                        Add Category <AddIcon/>
-                        
-                    </Button> */}
-                    <Button>
-                        <TextField
-                        InputProps={{
-                            style: {
-                                color: 'white'
-                            }
-                        }}
-                        />
-                    </Button>
-                </Box>
+            <Box sx={{display: 'flex', justifyContent: 'flex-end', margin: '2% 2%'}}>
                 <Link to={`/${userName}/addpost`}>
-                    <Button sx={{backgroundColor: '#4681f4', color: 'white'}}>
+                    <Button sx={{backgroundColor: '#4681f4', color: 'white', fontWeight: 'bold'}}>
                         Upload Post
-                    </Button>
-                    
+                    </Button>                    
                 </Link>
+            </Box>
+            <Box sx={{display: 'flex', flexDirection: 'row', gap: '50px', width: '75%', margin: '0 auto', color: 'white'}}>
+                {blogPostInfo && blogPostInfo.map((item, index)=>(   
+                        
+                        <div onClick={() => handleNavigatePostDetails(item.title)}className="post-container" key={index} sx={{display: 'flex', flexDirection: 'column'}}>
+                            <Box sx={{backgroundColor: '#1f1f23', borderRadius: '10px', }}>                                
+                                <img src={item.thumbnailImage} alt="" style={{width: '300px', height: '170px', borderRadius: '10px'}}/>
+
+                                <Typography variant="h5" sx={{textAlign: 'center', maxWidth: '300px'}}>
+                                    {item.title}                                 
+                                </Typography>
+
+                                <Typography sx={{ textAlign: 'center', color: '#C0C0C0', maxWidth: '300px'}} dangerouslySetInnerHTML={{ __html: item.postContent }} />
+
+                                <br/><hr/>         
+
+                                <Box sx={{display: 'flex', flexDirection: 'row', height: '30px', marginLeft: '10px', marginTop: '10px'}}>
+                                    <Avatar alt={`${userName}`} src={userProfileImage} style={{width: '25px', height: '25px', marginRight: '10px'}}/>
+                                    <Typography sx={{marginRight: '90px'}}>{userName}</Typography>
+                                    <FavoriteIcon sx={{color: 'red', width: '20px', height: '20px', marginRight: '5px'}}/> {item.noOfLikes}
+                                    <CommentIcon sx={{width: '20px', height: '20px', marginLeft: '10px', marginRight: '5px'}}/> {item.noOfComments}
+                                </Box>                                                                      
+                            </Box>              
+                        </div>
+                ))}
+
             </Box>
         </>
     )
