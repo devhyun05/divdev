0f63437b1d4cd4517ba5013222df10a0f8dd71ed
HyunSeongLee <devhyun05@gmail.com>
2024-01-06 03:25:04 -0500

    welcome page updates

--- frontend/src/pages/Welcome.jsx
+++ frontend/src/pages/Welcome.jsx
@@ -11,7 +11,7 @@ const Welcome = () => {
                 <Box sx={{display: 'flex', justifyContent: 'center', alignItems: 'center', width: '50%', height: '60%', marginLeft: '5%'}}> 
                     <Box className="box-container" sx={{display: 'flex', flexDirection: 'column'}}>
                         <Typography variant="h2">
-                            Blog style 
+                            Blog style!
                         </Typography>
                         <Typography variant="h2">
                             portfolio builder
